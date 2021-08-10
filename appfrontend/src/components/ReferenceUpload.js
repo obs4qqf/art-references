f32e0bd74e8bef274dabd6e877de73f783f2756d
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2021-08-10 11:04:28 -0400

    Began file upload to firebase

--- appfrontend/src/components/ReferenceUpload.js
+++ appfrontend/src/components/ReferenceUpload.js
@@ -4,24 +4,45 @@ import {storage} from '../firebase/firebase'
 
 const ReferenceUpload = () => {
     const [file, setFile] = useState(null)
+    const [fileName, setFileName] = useState(null)
+    const [error, setError] = useState(null)
+    const [url, setUrl] = useState(null)
 
     const onChangeFile = (e) => {
         setFile(e.target.files[0])
+        setFileName(e.target.files[0].name)
     }
 
 
     const uploadHandler = (e) => {
         e.preventDefault()
         console.log(file)
-        const storageRef = storage.ref(file.name);
-        storageRef.put(file)
+        const storageRef = storage.ref();
+        const fileRef = storageRef.child(`images/${file.name}`)
+        const uploadTask = fileRef.put(file)
+        uploadTask.on('state_changed', 
+            snapshot => {
+                console.log('Upload is occuring')
+            }, error => {
+                console.log(error.code)
+                setError(error.code)
+            }, async () => {
+                const newUrl = await uploadTask.snapshot.ref.getDownloadURL()
+                setUrl(newUrl)
+                setFileName(null)
+                setError(null)
+            })
     }
 
     return (
-        <form onSubmit={uploadHandler} >
-           <input type='file' name="file" onChange={onChangeFile}/>
-           <input type='submit' name="submit" value='Submit'/>
-        </form>
+        <>
+            <form onSubmit={uploadHandler} >
+                <input type='file' name="file" onChange={onChangeFile}/>
+                <input type='submit' name="submit" value='Submit'/>
+            </form>
+            {error && <div>{error}</div>}
+            {fileName && <div>{fileName}</div>}
+        </>
     )
 }
 
