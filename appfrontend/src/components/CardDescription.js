36f10839bb3071e403b28c3444c617465b536536
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2021-08-11 17:25:18 -0400

    ExpandedCard component styling

--- appfrontend/src/components/CardDescription.js
+++ appfrontend/src/components/CardDescription.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const CardDescription = () => {
     return (
-        <>
+        <div id="card-desc">
+            <h4>Description:</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore eos ea cum sunt sint ullam aliquam excepturi officia voluptates voluptatum. Facere a, doloribus molestias omnis ad reprehenderit aperiam voluptates quo!</p>
-        </>
+        </div>
     )
 }
 
--- appfrontend/src/components/ExpandedCard.js
+++ appfrontend/src/components/ExpandedCard.js
@@ -10,15 +10,17 @@ const ExpandedCard = ({clickedImage, minimizeImage, addTag, deleteTag}) => {
     }
 
     return (
-        <div id="expanded-card">
-            <div id="expanded-card-grid">
-                <img src={clickedImage.url} alt="Expanded Image" />
-                <i className="fas fa-times fa-3x" onClick={minimizeImage}></i>
-                <CardDescription id="card-desc"/>
-            </div>
-            <div className="tag-input-area">
-                <input type="text" onKeyDown={enterTag} />
-                <TagsList tags={clickedImage.tags} deleteTag={deleteTag} id={clickedImage.id}/>
+        <div id="expanded-card-background">
+            <div id="expanded-card">
+                <div className="tag-input-area">
+                    <input type="text" onKeyDown={enterTag} />
+                    <TagsList tags={clickedImage.tags} deleteTag={deleteTag} id={clickedImage.id}/>
+                </div>
+                <div>
+                    <img src={clickedImage.url} alt="Expanded Image" />
+                    <i className="fas fa-times fa-3x" onClick={minimizeImage}></i>
+                </div>
+                <CardDescription/>
             </div>
         </div>
     )
